d543a00a81fd1dd0754007013d09ba2483f83277
yaroslav414 <smartec414+2@gmail.com>
2025-03-07 22:25:39 +0200

    add admin page

--- src/app/api/articles/[id]/route.ts
+++ src/app/api/articles/[id]/route.ts
@@ -1,108 +1,150 @@
 import prisma from "@/PrismaDb/db";
 import { verifyToken } from "@/lib/generateToken";
+import { Article } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  context: { params: { id: string } }
+  {
+    params,
+  }: {
+    params: Promise<{ id: string }>;
+  }
 ) {
+  const { id } = await params;
+
   try {
-    const { id } = context.params;
-    const article = await prisma.article.findUnique({
-      where: { id: Number(id) },
+    let oneArt = await prisma.article.findUnique({
+      where: { id: +id },
       include: {
         comments: {
           include: {
             userComments: {
-              select: { id: true, username: true, email: true, isAdmin: true },
+              select: {
+                id: true,
+                username: true,
+                email: true,
+                isAdmin: true,
+              },
             },
           },
+
           orderBy: { createdAt: "desc" },
         },
       },
     });
-
-    if (!article) {
+    if (!oneArt) {
       return NextResponse.json(
-        { message: "Article not found" },
-        { status: 404 }
+        {
+          message: "article not found",
+        },
+        {
+          status: 404,
+        }
       );
     }
-    return NextResponse.json(article, { status: 200 });
+    return NextResponse.json(oneArt, {
+      status: 200,
+    });
   } catch (e) {
     return NextResponse.json(
-      { message: "Internal server error" },
-      { status: 500 }
+      { message: "internal server error" },
+      {
+        status: 500,
+      }
     );
   }
 }
-
 export async function PUT(
   request: NextRequest,
-  context: { params: { id: string } }
-) {
-  const { id } = context.params;
-  const article = await prisma.article.findUnique({
-    where: { id: Number(id) },
-  });
-
-  if (!article) {
-    return NextResponse.json({ message: "Article not found" }, { status: 404 });
+  {
+    params,
+  }: {
+    params: Promise<{ id: string }>;
   }
+) {
+  const { id } = await params;
 
-  const user = verifyToken(request);
-  if (!user || !user.isAdmin) {
+  let oneArt: Article | null = await prisma.article.findUnique({
+    where: { id: +id },
+  });
+  if (!oneArt) {
     return NextResponse.json(
-      { message: "Unauthorized access denied" },
+      {
+        message: "article not found",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+  const user = verifyToken(request);
+  if (!user || user.isAdmin == false) {
+    return NextResponse.json(
+      { message: "unauthorized access denied" },
       { status: 401 }
     );
   }
-
-  const body = await request.json();
-  const updatedArticle = await prisma.article.update({
-    where: { id: Number(id) },
-    data: { title: body.title, desc: body.desc },
+  const body: { title: string; desc: string } = await request.json();
+  let updateArt: Article = await prisma.article.update({
+    where: { id: +id },
+    data: {
+      title: body.title,
+      desc: body.desc,
+    },
   });
-
-  return NextResponse.json(updatedArticle, { status: 200 });
+  return NextResponse.json(updateArt, {
+    status: 200,
+  });
 }
-
 export async function DELETE(
   request: NextRequest,
-  context: { params: { id: string } }
+  {
+    params,
+  }: {
+    params: Promise<{ id: string }>;
+  }
 ) {
+  const { id } = await params;
   try {
-    const { id } = context.params;
-    const article = await prisma.article.findUnique({
-      where: { id: Number(id) },
-      include: { comments: true },
+    let oneArt = await prisma.article.findUnique({
+      where: { id: +id },
+      include: {
+        comments: true,
+      },
     });
-
-    if (!article) {
+    if (!oneArt) {
       return NextResponse.json(
-        { message: "Article not found" },
-        { status: 404 }
+        {
+          message: "article not found",
+        },
+        {
+          status: 404,
+        }
       );
     }
-
     const user = verifyToken(request);
-    if (!user || !user.isAdmin) {
+    if (!user || user.isAdmin == false) {
       return NextResponse.json(
-        { message: "Unauthorized access denied" },
+        { message: "unauthorized access denied" },
         { status: 401 }
       );
     }
-
-    await prisma.article.delete({ where: { id: Number(id) } });
-    await prisma.comment.deleteMany({
-      where: { id: { in: article.comments.map((c) => c.id) } },
-    });
-
-    return NextResponse.json({ message: "Article deleted" }, { status: 200 });
+    await prisma.article.delete({ where: { id: +id } });
+    const comments = oneArt.comments.map((comment) => comment.id);
+    await prisma.comment.deleteMany({ where: { id: { in: comments } } });
+    return NextResponse.json(
+      { message: "article deleted" },
+      {
+        status: 200,
+      }
+    );
   } catch (e) {
     return NextResponse.json(
-      { message: "Internal server error" },
-      { status: 500 }
+      { message: "internal server error" },
+      {
+        status: 500,
+      }
     );
   }
 }
