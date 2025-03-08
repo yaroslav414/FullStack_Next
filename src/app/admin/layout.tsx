88fae51b06a5d4a7a38fd24f496e5b135509f4de
yaroslav414 <smartec414+2@gmail.com>
2025-03-08 12:07:48 +0200

    edit some bugs

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -6,15 +6,11 @@ export default function AdminLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className="scroll-smooth" lang="en">
-      <body className={` overflow-x-hidden antialiased`}>
-        <div className="flex justify-between items-start  mt-20">
-          <div className="w-16 lg:w-1/5">
-            <AdminSidebar />
-          </div>
-          <div className="w-full lg:w-4/5 mx-4 sm:mx-0">{children}</div>
-        </div>
-      </body>
-    </html>
+    <div className="flex justify-between items-start  mt-20">
+      <div className="w-16 lg:w-1/5">
+        <AdminSidebar />
+      </div>
+      <div className="w-full lg:w-4/5 mx-4 sm:mx-0">{children}</div>
+    </div>
   );
 }
