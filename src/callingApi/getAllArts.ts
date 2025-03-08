d61d6d4f501fecfe685e21763140afbc458cfba1
yaroslav414 <smartec414+2@gmail.com>
2025-03-08 16:40:43 +0200

    connect with backend and make arts and pagination and search page

--- src/callingApi/getAllArts.ts
+++ src/callingApi/getAllArts.ts
@@ -5,20 +5,15 @@ export const getAllArts = async ({
 }: {
   page: string;
 }): Promise<{ data: Article[]; length: number; lengthPerPage: number }> => {
-  try {
-    const response = await fetch(
-      `http://localhost:3000/api/articles?page=${page}`
-    );
+  const response = await fetch(
+    `http://localhost:3000/api/articles?page=${page}`
+  );
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.error("Error fetching articles:", error);
-    return { data: [], length: 0, lengthPerPage: 0 };
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
   }
+
+  return await response.json();
 };
 // {{Domain}}/api/articles/search?searchText=no
 export const getArtsBySearch = async ({
